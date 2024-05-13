ee99842920259658da9b1325b2ce3195d16d1277
Jorge Tome Blanco <jorge.tome@wolterskluwer.com>
2024-05-13 23:55:05 +0200

    AddBuilders implemented

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -1,12 +1,32 @@
-﻿using Builder4You.Interfaces;
+﻿using Builder4You.Implementations;
+using Builder4You.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Builder4You.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        private static (Type Class, Type Interface) BuilderType = (typeof(Builder<>), typeof(IBuilder<>));
+        private static (Type Class, Type Interface) BuilderFromType = (typeof(BuilderFrom<>), typeof(IBuilderFrom<>));
         public static IServiceCollection AddBuilders(this IServiceCollection services)
         {
+            var buildeableType = typeof(IBuildeable);
+
+            var buildeableClasses = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(IsBuildeableClass);
+
+            foreach (var buildeableClass in buildeableClasses)
+            {
+                services.AddScoped(
+                    BuilderType.Interface.MakeGenericType(buildeableClass),
+                    BuilderType.Class.MakeGenericType(buildeableClass));
+
+                services.AddScoped(
+                    BuilderFromType.Interface.MakeGenericType(buildeableClass),
+                    BuilderFromType.Class.MakeGenericType(buildeableClass));
+            }
+
             return services;
         }
         public static IServiceCollection AddProjectables(this IServiceCollection services)
@@ -37,6 +57,16 @@ namespace Builder4You.Extensions
 
             return t.GetInterfaces().Any(
                 i => i.Name == projectableType.Name);
+        }
+
+        private static bool IsBuildeableClass(Type t)
+        {
+            if (!t.IsClass || t.IsAbstract) return false;
+
+            var buildeableType = typeof(IBuildeable);
+
+            return t.GetInterfaces().Any(
+                i => i.Name == buildeableType.Name);
         }
     }
 }
